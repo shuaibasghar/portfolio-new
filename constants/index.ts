a0677c082e5494b54928c82e6914fc93a1411074
shuaibasghar <shuaibasghar@gmail.com>
2023-12-27 15:05:53 +0500

    navbard added

---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: "/", key: "home", label: "Home" },
+    { href: "/expertise", key: "expertise", label: "Expertise" },
+    { href: "/work", key: "work", label: "Work" },
+    { href: "/experience", key: "experience", label: "Experience" },
+    { href: "/contact", key: "contact", label: "Contact" },
+];
